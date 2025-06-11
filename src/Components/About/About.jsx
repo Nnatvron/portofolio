3f85bdeee3897713b6dfa7552e618e3e26116a41
Nnatvron <natravelsitra07@gmail.com>
2025-06-11 00:12:14 -0700

    update v7

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,7 +14,6 @@ function About() {
             </div>
 
             <div className={AboutCSS.about_info}>
-                <p data-aos="fade-left" data-aos-duration="1500" data-aos-delay="400">About Me</p>
                 <h2 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="600">Natravell Sitra</h2>
                 <h3 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="800">Hi I am <span>Front End</span> Developer</h3>
                 <p data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000">Saya adalah Natravell Sitra, seorang siswa yang menekuni bidang Teknik Komputer dan Jaringan dengan fokus pada pengembangan Front-End dan Design Web. Saya memiliki tekad yang kuat untuk terus belajar dan mengasah keterampilan saya demi mencapai kesempurnaan. Selalu berusaha untuk menemukan solusi terbaik dalam setiap projek yang saya kerjakan dan memiliki minat besar di bidang Front-End dan Design Web.</p>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,26 +7,77 @@ function Header(){
     AOS.init();
     return(
         <>
-        <section id="home" >
-            <div className={HeaderCSS.hero} >
-                <div className={HeaderCSS.hero_info} >
-                    <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1000">Hi, I am<span> Natravell Sitra </span></h1>
-                    <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1100">"The sky is the limit for those who are not afraid to fly."</p>
+        <section id="home">
+            <div className={HeaderCSS.hero}>
+                <div className={HeaderCSS.glass_effect}></div>
+                
+                <div className={HeaderCSS.hero_info}>
+                    <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1000">
+                        Hi, I am<span> Natravell Sitra </span>
+                    </h1>
+                    <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1100">
+                        "The sky is the limit for those who are not afraid to fly."
+                    </p>
 
                     <div className={HeaderCSS.social_icons}>
-                        <i> <a href="https://www.instagram.com/natar.05" className="fa-brands fa-instagram" data-aos="fade" data-aos-duration="1500" data-aos-delay="1200"></a></i>
-                        <i> <a href="https://www.facebook.com/natra.natra.3154/" className="fa-brands fa-facebook" data-aos="fade" data-aos-duration="1500" data-aos-delay="1300"></a></i>
-                        <i> <a href="https://www.linkedin.com/in/natravell-sitra-99994829b?" className="fa-brands fa-linkedin" data-aos="fade" data-aos-duration="1500" data-aos-delay="1400"></a></i>
-                        <i> <a href="https://github.com/Nnatvron" className="fa-brands fa-github" data-aos="fade" data-aos-duration="1500" data-aos-delay="1500"></a></i>
+                        <i> 
+                            <a href="https://www.instagram.com/natar.05" 
+                               className="fa-brands fa-instagram" 
+                               data-aos="fade" 
+                               data-aos-duration="1500" 
+                               data-aos-delay="1200">
+                            </a>
+                        </i>
+                        <i> 
+                            <a href="https://www.facebook.com/natra.natra.3154/" 
+                               className="fa-brands fa-facebook" 
+                               data-aos="fade" 
+                               data-aos-duration="1500" 
+                               data-aos-delay="1300">
+                            </a>
+                        </i>
+                        <i> 
+                            <a href="https://www.linkedin.com/in/natravell-sitra-99994829b?" 
+                               className="fa-brands fa-linkedin" 
+                               data-aos="fade" 
+                               data-aos-duration="1500" 
+                               data-aos-delay="1400">
+                            </a>
+                        </i>
+                        <i> 
+                            <a href="https://github.com/Nnatvron" 
+                               className="fa-brands fa-github" 
+                               data-aos="fade" 
+                               data-aos-duration="1500" 
+                               data-aos-delay="1500">
+                            </a>
+                        </i>
                     </div>
 
                     <div className={HeaderCSS.hero_btns}>
-                        <button> <a href="https://wa.me/6285882494679" data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1600">Hire Me</a></button>
-                        <button> <a href="#contact" data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1700">Contact</a></button>
+                        <button>
+                            <a href="https://wa.me/6285882494679" 
+                               data-aos="fade-right" 
+                               data-aos-duration="1500" 
+                               data-aos-delay="1600">
+                                Hire Me
+                            </a>
+                        </button>
+                        <button>
+                            <a href="#contact" 
+                               data-aos="fade-right" 
+                               data-aos-duration="1500" 
+                               data-aos-delay="1700">
+                                Contact
+                            </a>
+                        </button>
                     </div>
                 </div>
 
-                <div className={HeaderCSS.hero_img} data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1800">
+                <div className={HeaderCSS.hero_img} 
+                     data-aos="fade-left" 
+                     data-aos-duration="1500" 
+                     data-aos-delay="1800">
                     <img src={Hero} alt="Hero_img"/>
                 </div>
             </div>
